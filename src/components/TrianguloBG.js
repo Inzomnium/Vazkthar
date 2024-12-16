d150349e6112cd9cee97d1536795574fa7ffd3c9
Inzomnium <96298000+Inzomnium@users.noreply.github.com>
2024-12-16 12:43:31 +0100

    fix useAnimate

--- src/components/TrianguloBG.js
+++ src/components/TrianguloBG.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { motion } from 'framer-motion';
-
 
 // Función para generar una posición aleatoria
 const getRandomBackgroundPosition = () => {
@@ -10,8 +9,9 @@ const getRandomBackgroundPosition = () => {
 };
 
 function getRandomAniDur() {
-    return Math.floor(Math.random() * (40 - 10 + 1)) + 10;
-  }
+  return Math.floor(Math.random() * (40 - 10 + 1)) + 10;
+}
+
 const TriangleBackground = () => {
   const [dimensions, setDimensions] = useState({ rows: 0, cols: 0 });
   const [backgroundPositions, setBackgroundPositions] = useState([]);
@@ -19,7 +19,8 @@ const TriangleBackground = () => {
   const horizontalMargin = -25; // Margen horizontal negativo aplicado en CSS
   const verticalMargin = 2; // Margen vertical aplicado en CSS
 
-  const calculateGrid = () => {
+  // Memoriza la función calculateGrid con useCallback
+  const calculateGrid = useCallback(() => {
     const effectiveWidth = size + horizontalMargin;
     const effectiveHeight = size / 2 + verticalMargin;
 
@@ -29,71 +30,69 @@ const TriangleBackground = () => {
     setDimensions({ rows, cols });
 
     // Generar posiciones aleatorias para cada triángulo
-    const totalTriangles = (rows + 1) * (cols + 1); // Incluye los triángulos y medios triángulos
+    const totalTriangles = (rows + 1) * (cols + 1);
     const positions = Array.from({ length: totalTriangles }, () => getRandomBackgroundPosition());
     setBackgroundPositions(positions);
-  };
+  }, [size, horizontalMargin, verticalMargin]);
 
+  // Agregar calculateGrid como una dependencia
   useEffect(() => {
     calculateGrid();
     window.addEventListener('resize', calculateGrid);
     return () => window.removeEventListener('resize', calculateGrid);
-  }, []);
+  }, [calculateGrid]);
 
   return (
-    <div className='container-canvas'>
-    <div className="triangle-grid">
-      {Array.from({ length: dimensions.rows }).map((_, rowIndex) => {
-        const lastTriangleInverted = (rowIndex + dimensions.cols - 1) % 2 === 1;
+    <div className="container-canvas">
+      <div className="triangle-grid">
+        {Array.from({ length: dimensions.rows }).map((_, rowIndex) => {
+          const lastTriangleInverted = (rowIndex + dimensions.cols - 1) % 2 === 1;
 
-        return (
-          
-          <div className="triangle-row" key={rowIndex}>
-            {Array.from({ length: dimensions.cols }).map((_, colIndex) => {
-              const isInverted = (rowIndex + colIndex) % 2 === 1;
-              const controlIndex = rowIndex * dimensions.cols + colIndex;
+          return (
+            <div className="triangle-row" key={rowIndex}>
+              {Array.from({ length: dimensions.cols }).map((_, colIndex) => {
+                const isInverted = (rowIndex + colIndex) % 2 === 1;
+                const controlIndex = rowIndex * dimensions.cols + colIndex;
 
-              return (
-                <motion.div
-                  className={`triangle ${isInverted ? 'inverted' : ''}`}
-                  key={`${rowIndex}-${colIndex}`}
-                  style={{
-                    backgroundPosition: backgroundPositions[controlIndex] || 'center',
-                    backgroundImage:
-                      "url('https://cdn2.hubspot.net/hubfs/2351684/Imported_Blog_Media/File-formats-and-sizes-image-1000.jpg')",
-                  }}
-                  animate={{
-                    backgroundPosition: [
-                      getRandomBackgroundPosition(),
-                      getRandomBackgroundPosition(),
-                    ],
-                    transition: { 
-                        duration: getRandomAniDur()
-                        , repeat: Infinity, ease: 'linear' },
-                  }}
-                />
-              );
-            })}
-            <motion.div
-              className={`half-triangle ${lastTriangleInverted ? 'inverted' : ''}`}
-              style={{
-                backgroundPosition:
-                  backgroundPositions[rowIndex * dimensions.cols + dimensions.cols] || 'center',
-                backgroundImage:
-                  "url('https://cdn2.hubspot.net/hubfs/2351684/Imported_Blog_Media/File-formats-and-sizes-image-1000.jpg')",
-              }}
-              animate={{
-                backgroundPosition: [
-                  getRandomBackgroundPosition(),
-                  getRandomBackgroundPosition(),
-                ],
-                transition: { duration: 40, repeat: Infinity, ease: 'linear' },
-              }}
-            />
-          </div>
-        );
-      })}
-    </div>
+                return (
+                  <motion.div
+                    className={`triangle ${isInverted ? 'inverted' : ''}`}
+                    key={`${rowIndex}-${colIndex}`}
+                    style={{
+                      backgroundPosition: backgroundPositions[controlIndex] || 'center',
+                      backgroundImage:
+                        "url('https://cdn.carredartistes.com/en-fr/content_images/watercolor%20painting%20(2).png')",
+                    }}
+                    animate={{
+                      backgroundPosition: [
+                        getRandomBackgroundPosition(),
+                        getRandomBackgroundPosition(),
+                      ],
+                      transition: { duration: getRandomAniDur(), repeat: Infinity, ease: 'linear' },
+                    }}
+                  />
+                );
+              })}
+              <motion.div
+                className={`half-triangle ${lastTriangleInverted ? 'inverted' : ''}`}
+                style={{
+                  backgroundPosition:
+                    backgroundPositions[rowIndex * dimensions.cols + dimensions.cols] || 'center',
+                  backgroundImage:
+                    "url('https://cdn2.hubspot.net/hubfs/2351684/Imported_Blog_Media/File-formats-and-sizes-image-1000.jpg')",
+                }}
+                animate={{
+                  backgroundPosition: [
+                    getRandomBackgroundPosition(),
+                    getRandomBackgroundPosition(),
+                  ],
+                  transition: { duration: 40, repeat: Infinity, ease: 'linear' },
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
