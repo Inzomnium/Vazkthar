2f44e565e1128ac2a29c5f1e55edb3008003d479
Inzomnium <96298000+Inzomnium@users.noreply.github.com>
2025-06-26 16:45:30 +0200

    nuevas imagenes

---
+++ works-static.js
@@ -0,0 +1,185 @@
+module.exports = [
+  {
+    "slug": "a-men-is-a-boy-with-a-thicker-helmet"
+  },
+  {
+    "slug": "accuser-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "aligned-with-the-moon"
+  },
+  {
+    "slug": "an-ode-to-cats-001"
+  },
+  {
+    "slug": "an-ode-to-cats-002"
+  },
+  {
+    "slug": "an-ode-to-cats-003"
+  },
+  {
+    "slug": "an-ode-to-cats-004"
+  },
+  {
+    "slug": "an-ode-to-cats-005"
+  },
+  {
+    "slug": "an-ode-to-cats-006"
+  },
+  {
+    "slug": "conditions-to-bloom-in-rust-003"
+  },
+  {
+    "slug": "conditions-to-bloom-in-rust-006"
+  },
+  {
+    "slug": "conditions-to-bloom-in-rust-007"
+  },
+  {
+    "slug": "conditions-to-bloom-in-the-ashes"
+  },
+  {
+    "slug": "conditions-to-bloom-in-winter-011"
+  },
+  {
+    "slug": "conditions-to-bloom-in-winter-part-003"
+  },
+  {
+    "slug": "conditions-to-bloom-in-winter-part-033"
+  },
+  {
+    "slug": "conditions-to-bloom-in-winter-part-111"
+  },
+  {
+    "slug": "conditions-to-bloom-in-your-memories-003"
+  },
+  {
+    "slug": "conditions-to-bloom-in-your-memories-004"
+  },
+  {
+    "slug": "conditions-to-bloom-in-your-memories-005"
+  },
+  {
+    "slug": "conditions-to-bloom-in-zenith-020"
+  },
+  {
+    "slug": "corazon-delator-i"
+  },
+  {
+    "slug": "corazon-delator-ii"
+  },
+  {
+    "slug": "cry-havoc-and-let-slip-the-dogs-of-war"
+  },
+  {
+    "slug": "diablada-chilena"
+  },
+  {
+    "slug": "erosion-of-id"
+  },
+  {
+    "slug": "flowers-over-missiles"
+  },
+  {
+    "slug": "harvester-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "loops-and-anxiety-2"
+  },
+  {
+    "slug": "loops-and-anxiety"
+  },
+  {
+    "slug": "modern-needs-meets-modern-audience"
+  },
+  {
+    "slug": "of-war-and-other-obsolete-games"
+  },
+  {
+    "slug": "overseer-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "reaper-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "sentry-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "signal-async"
+  },
+  {
+    "slug": "skulls-and-checks"
+  },
+  {
+    "slug": "sower-of-thy-algorithm-overlords-domains"
+  },
+  {
+    "slug": "the-1st-seal-or-the-core-pulse"
+  },
+  {
+    "slug": "the-2nd-seal-or-bloom-of-protocols"
+  },
+  {
+    "slug": "the-3th-seal-or-encoded-reliquary"
+  },
+  {
+    "slug": "the-4th-seal-or-the-crown-of-silence"
+  },
+  {
+    "slug": "the-blind-march-of-progress"
+  },
+  {
+    "slug": "the-harvester-of-the-fog"
+  },
+  {
+    "slug": "the-immortality-of-an-overthinking-beauty"
+  },
+  {
+    "slug": "the-immortality-of-contempt-with-a-look"
+  },
+  {
+    "slug": "the-immortality-of-her-infinite-cosmos"
+  },
+  {
+    "slug": "the-immortality-of-imperfect-beauty"
+  },
+  {
+    "slug": "the-immortality-of-the-lost-fragments-of-yourself"
+  },
+  {
+    "slug": "the-immortality-of-the-muse-that-i-remember"
+  },
+  {
+    "slug": "the-immortality-the-permanent-memories"
+  },
+  {
+    "slug": "the-last-ride-of-the-vakzthari"
+  },
+  {
+    "slug": "the-listener-wears-the-crown-of-silence"
+  },
+  {
+    "slug": "the-self-view"
+  },
+  {
+    "slug": "the-tainted-operator"
+  },
+  {
+    "slug": "the-vakzthar-from-another-cycle"
+  },
+  {
+    "slug": "the-vakzthari-operator-from-the-past"
+  },
+  {
+    "slug": "the-vakzthari-operator"
+  },
+  {
+    "slug": "there-are-flowers-in-the-interference"
+  },
+  {
+    "slug": "to-let-it-all-out"
+  },
+  {
+    "slug": "zoology-from-the-dream-world"
+  }
+]
